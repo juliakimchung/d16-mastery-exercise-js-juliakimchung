6225d281ca5f6ea618a539c8d211924e86156db0
juliakimchung <julhkm@gmail.com>
2016-10-30 22:10:03 -0500

    tree updated innerHTML

--- quiz.js
+++ quiz.js
@@ -2,28 +2,33 @@
 var height =document.getElementById("height");
 var character = document.getElementById("character");
 var treeButton = document.getElementById("keypress-input");
+var yourTree = document.getElementById("yourTree");
 
-
-function growYourTree(tree) {
+function growYourTree(a, b) {
 	//console.log("tree", tree );
 		tree = {
 			height: height.value,
 			character: character.value
 		}
+	var inject = document.getElementById("yourTree");
 	  
 	  	
-	for (var i = 1; i <= tree.height;  i++ ) {
+	for (var i = 1; i < tree.height;  i++ ) {
 		
 	  	var inputChar = tree.character;
-
-		console.log (" ".repeat(height.value--) + inputChar.repeat((i*2)-1));
- }
- }
-
+	  	// var myTree =  " ".repeat(height.value--) + inputChar.repeat((i*2)-1) ;
+	  	// console.log(myTree);
+	//console.log (" ".repeat(height.value--) + inputChar.repeat((i*2)-1));
+	inject.innerHTML += "&nbsp".repeat(height.value--) + inputChar.repeat((i*2)-1);
+	// var myTree =  " ".repeat(height.value--) + inputChar.repeat((i*2)-1) ;
 
 	
+	}
+}         
+		
 
- 
+
+
 function checkInput () {
 	
 	if (height.value && character.value) {
@@ -36,7 +41,6 @@ function checkInput () {
 		alert("Both fields must have a value");
 	}
 }
+// " ".repeat(height.value--
 
-
- treeButton.addEventListener("click", checkInput);
- 
+ treeButton.addEventListener("click", checkInput); 
