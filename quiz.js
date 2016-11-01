2fdb0abc0d8646b3efac45dc6d365da19b0cc810
juliakimchung <julhkm@gmail.com>
2016-11-01 15:54:29 -0500

    really done making trees

--- quiz.js
+++ quiz.js
@@ -16,26 +16,51 @@ function growYourTree(a, b) {
 	for (var i = 1; i < tree.height;  i++ ) {
 		
 	  	var inputChar = tree.character;
-	  	// var myTree =  " ".repeat(height.value--) + inputChar.repeat((i*2)-1) ;
-	  	// console.log(myTree);
-	//console.log (" ".repeat(height.value--) + inputChar.repeat((i*2)-1));
-	inject.innerHTML += "&nbsp".repeat(height.value--) + inputChar.repeat((i*2)-1);
-	// var myTree =  " ".repeat(height.value--) + inputChar.repeat((i*2)-1) ;
+	  	 var myTree =  " ".repeat(height.value--) + inputChar.repeat((i*2)-1) ;
+	  	 console.log(myTree);
 
+		// yourTree.addEventListener("keypress", function(event){
+
+		// if (event.target.keycode === 13) {
+			
+		// console.log(myTree);
 	
-	}
-}         
+	};
+	// character.addEventListener("keypress", function(event) {
+			
+	// 	if (event.keyCode === 13) {
+
+	// 		growYourTree(a, b);
+			
+	// 	console.log(myTree);
+	// }
+}
+	
+ 
+ character.addEventListener("keypress", function(event){
+			
+ 		if (event.keyCode === 13) {
+			
+ 			growYourTree(height.value, character.value);
+			
 		
+ 	};
+
+ })     		
+	// textInput.addEventListener("keypress", function(event) {
+ //      if (event.keyCode === 13) {
+ //        Chatty.addNewMessage(textInput.value);
+ //      }
 
 
 
 function checkInput () {
 	
 	if (height.value && character.value) {
-		//tree.height = height.value;
+		//tree.height = height.value
 		//tree.character = character.value;
 		growYourTree ();
-	} else {
+	} else  {
 
 
 		alert("Both fields must have a value");
@@ -44,3 +69,5 @@ function checkInput () {
 // " ".repeat(height.value--
 
  treeButton.addEventListener("click", checkInput); 
+
+ 
